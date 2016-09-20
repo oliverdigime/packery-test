d9e224e780605fcac82f17b740270d9a2caa09bf
Oliver <oliver@digi.me>
2016-09-20 10:45:48 +0100

    updates

---
+++ me.digi/main.ts
@@ -0,0 +1,36 @@
+declare var Packery:any;
+
+ /**
+ * main
+ */
+class main 
+{
+    private _packery:Packery;
+    private _grid:any;
+
+    constructor() 
+    {
+    }
+
+    init()
+    {
+        alert("init called");
+        this._grid = document.querySelector('.grid');
+        this._grid.addEventListener('click', this.clickHandler);
+        this._packery = new Packery(this._grid, 
+        {
+            // options
+            itemSelector: '.grid-item',
+            gutter: 10
+        });
+    }
+
+    clickHandler()
+    {
+        alert("click called");
+        // remove clicked element
+        this._packery.remove( event.target );
+        // shiftLayout remaining item elements
+        this._packery.shiftLayout();
+    }
+}
